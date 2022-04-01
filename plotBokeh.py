f68cf5ff1e53f1477cdafd89cd6249b75dd9a39a
laeti <laeticormier@gmail.com>
2022-04-01 15:50:54 +0200

    background

--- plotBokeh.py
+++ plotBokeh.py
@@ -7,6 +7,7 @@ import pandas as pd
 from bokeh.models import NumeralTickFormatter
 from bokeh.models import DatetimeTickFormatter
 from datetime import datetime
+from bokeh.io import curdoc
 
 #récuperation des données 
 
@@ -39,7 +40,7 @@ source = ColumnDataSource(data=df)
 
 # # create a plot using the ColumnDataSource's two columns
 p = figure(title="Bitcoin Values",
-        sizing_mode="stretch_width",    
+        sizing_mode="stretch_both",    
         plot_width=900, 
         plot_height=800, 
         x_axis_label='Times (min) ', 
@@ -52,7 +53,6 @@ p = figure(title="Bitcoin Values",
 output_file("index.html")
 p.title.align = 'center'
 p.title.text_font_size = '15pt'
-
 DTF =  DatetimeTickFormatter()
 DTF.hours = ["%H:%M"]
 DTF.days = ["%d/%m/%Y"]
@@ -86,8 +86,8 @@ p.xaxis[0].formatter = DTF
 
 
 
-p.background_fill_color = ('#cccccc')
-p.outline_line_color = (0, 0, 255)
+p.background_fill_color = ('#2F2F2F')
+p.outline_line_color = (255, 255, 255)
 show(p)
 
 
