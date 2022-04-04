cc55559936a3f3e34702fb17d3caf70c85c09d07
laeti33 <102666341+laeti33@users.noreply.github.com>
2022-04-04 17:32:33 +0200

    Update sendMail.py

--- sendMail.py
+++ sendMail.py
@@ -18,7 +18,7 @@ count=0
 for i in range(len(df)-1):
     if df.iloc[i,1] < df.iloc[i+1,1]:
         count+=1
-        if count==5:
+        if count==-5:
             subprocess.run(["python3 mailUp.py"])
 
     if df.iloc[i,1] > df.iloc[i+1,1]:
